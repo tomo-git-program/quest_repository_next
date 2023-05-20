7c2af28ccc8d0184209a06c564244692518ca74d
tomo-git-program <touhouchunanika@gmail.com>
2023-05-20 14:57:30 +0900

    オリジナルプロダクトの書き直し、flowchartの追加

---
+++ week7-8/level_1_javaScript/hello.js
@@ -0,0 +1,29 @@
+"use strict";
+
+
+
+/*
+  複数行コメント
+*/
+
+/*
+/*
+  ネストされたコメントは書けない
+*/
+// */ ←ここでエラー
+
+
+
+
+
+console.log("hello js!");
+
+const sum1 = 5;
+
+const sum2 = 3;
+
+let sum = sum1 + sum2
+
+let difference = sum1 - sum2
+
+console.log(sum + difference);
